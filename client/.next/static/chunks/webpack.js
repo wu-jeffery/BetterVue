9ea74880fdad785c1926f5f255d9493319a76e6f
Jason Lin <linj2314@gmail.com>
2024-09-28 15:29:23 -0400

    fixing stuff

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "def5228b60c1a8d5"; }
+/******/ 		__webpack_require__.h = function() { return "95584a982dc26f24"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       	<h1 className="text-center text-6xl">Interview Prep Website</h1>
 		<Link href="/login/" className='rounded '>
 			Go to login
-		</Link>
+		</Link> //
     </div>
   );
 }
