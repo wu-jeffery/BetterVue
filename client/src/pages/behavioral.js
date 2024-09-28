c9a0097c2ae9f60b83e9abb5932c81e37164d870
Jonathan <jhchen360@gmail.com>
2024-09-28 16:49:37 -0400

    stuff

--- client/src/pages/behavioral.js
+++ client/src/pages/behavioral.js
@@ -4,12 +4,13 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      	<h1 className="text-center text-6xl">Behavioral Practice</h1>
-        <Link href="/login/" className='m-5'>
-            Create an account to start practicing!
-        </Link>
+      	<h1 className="text-center items-center justify-center text-6xl font-extrabold my-5">Behavioral Practice</h1>
+        <div className='p-5 border'>
+            <Link href="/login/">
+                Create an account to start practicing!
+            </Link>
+        </div>
         <Link href="/" className='m-5'>
-            <Image> </Image>
             Home
         </Link>
     </div>
