b2de4b6a14e58941191cdedb0fb2daf7ba67c7ee
tyleryee <yeetyler25@gmail.com>
2024-09-28 15:58:48 -0400

    b

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e38d325b38990d23"; }
+/******/ 		__webpack_require__.h = function() { return "01a165f97469ca56"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/src/pages/behavioral.js
+++ client/src/pages/behavioral.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       	<h1 className="text-center text-6xl font-extrabold my-5">Behavioral Practice</h1>
-        <div className='my-10 mx-20 border-red-50 border-spacing-5'>
+        <div className='my-10 mx-20 border-spacing-5'>
             <Link href="/login/">
                 Create an account to start practicing!w
             </Link>
