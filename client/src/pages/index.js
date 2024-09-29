4a76e07c86387521f12a537a7fe8e017f409562c
Jason Lin <linj2314@gmail.com>
2024-09-29 08:56:23 -0400

    changes

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,17 +4,21 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col items-center">
-      	<h1 className="flex h-1/4 text-center text-6xl items-center justify-center">top text</h1>
-		<div className='h-full w-full flex flex-col items-center justify-center space-y-10'>
-			<Link href="/login/" className='rounded border text-xl p-5 w-1/4 text-center'>
-				Go to login
-			</Link>
-			<Link href="/create_account/" className='rounded border text-xl p-5 w-1/4 text-center'>
-				Create an account!
-			</Link>
-			<Link href="/behavioral/" className='rounded border text-xl p-5 w-1/4 text-center'>
-				Practice behaviorals!
-			</Link>
+		<div className="h-1/6 w-full flex flex-row justify-between">
+			<h1 className="flex w-1/2 p-6 pl-20 text-3xl items-center">BetterVue</h1>
+			<div className="w-1/4"></div>
+			<div className="w-1/4 flex flex-row space-x-5 justify-center items-center">
+				<Link href="/login/" className='p-5'>
+					Login
+				</Link>
+				<Link href="/create_account/" className='p-5'>
+					Register
+				</Link>
+			</div>
+		</div>
+      	
+		<div className='h-1/4 w-1/3 text-center text-4xl flex flex-col items-center justify-center space-y-10'>
+			Practice behavioral interviews or race in technical interviews
 		</div>
 		
     </div>
